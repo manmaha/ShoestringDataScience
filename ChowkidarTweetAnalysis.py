8adfd28418e5870a79586d09625b59ef450b8639
manmaha <manish.p.mahajan@gmail.com>
2019-05-23 13:31:25 +0800

    Added religious regex searches

--- ChowkidarTweetAnalysis.py
+++ ChowkidarTweetAnalysis.py
@@ -59,13 +59,27 @@ for user in chowkidars:
 total_chowkidars = len(chowkidars)
 total_tweets = len(tweets)
 
+import pandas as pd
 df = pd.DataFrame(tweets, columns=['text'])
-nehruvianQuery = 'Nehru|Rajiv|Jwahar[lal]| Jawahr[lal]|Indira|Jawahar[lal]|Bofors|Bofars|जवाहरलाल|नेहरु|राजीव|इन्दिरा|इन्दीरा'
-nehruvianTweets = df['text'].str.contains(nehruvianQuery,case=False, regex=True).value_counts()
+
+#Build up queries
+nehru = 'Nehru|Jwahar(lal)?| Jawahr(lal)?|Jawahar(lal)?|जवाहरलाल|जवाहर(लाल)?|नेहरु'
+indira = 'Indira|इन्दिरा|इन्दीरा'
+rajiv = 'Rajiv|Bofors|Bofars|राजीव|बोफोर्स|बोफर्स'
+others = ''
+
+
+religious_query = religious_query = 'Hindu(s)?($|\b)|Hinduism|Hindutv|Muslim(s)?|Moslem(s)?|Islam|Muss?alman(s)|Mohammaden(s)?|मुस्लिम|इस्लाम|मुस्सलमान|मुस्लमान'
+
+
+nehruvian_query = nehru+'|'+indira+'|'+rajiv
+nehruvian_tweets = df['text'].str.contains(nehruvian_query,case=False, regex=True).value_counts()
+religious_query = 'Hindu(s)?($|\b)|Hinduism|Hindutv|Dharm(a)?|Muslim(s)?|Moslem(s)?|Islam|Muss?alman(s)|Mohammaden(s)?|धर्म|मुस्लिम|इस्लाम|मुस्सलमान|मुस्लमान'
+religious_tweets = df['text'].str.contains(religious_query,case=False, regex=True).value_counts()
 
 #lets check the number of Devnagri Tweets, query2 is the unicode list of the devnagri language
-devnagriQuery = '[\u0900-\u097F]'
-devnagri= df['text'].str.contains(devnagriQuery,case=False, regex=True).value_counts()
+devnagri_query = '[\u0900-\u097F]'
+devnagri= df['text'].str.contains(devnagri_query,case=False, regex=True).value_counts()
 
 #Lets do some reporting
 print("I was able to extract {:d} Chowkidars most active on Twitter in the last few days\n".format(total_chowkidars))
@@ -73,11 +87,10 @@ print("Top 10 Twitter Handles are:\n")
 for user in chowkidars[0:10]:
       print(user.screen_name)
 print("\n{:.2f}% of {:d} Chowkidar Tweets extracted are in Devnagri".format(devnagri[1]/total_tweets*100,total_tweets))
-print("\nI queried for Tweets containing Nehru,Indira,Rajiv,Jawaharlal and Bofors\n")
-print("\n{:.2f}% of {:d} Chowkidar Tweets refer to dead Nehru-Gandhis".format(nehruvianTweets[1]/total_tweets*100,total_tweets))
-
-
-# In[81]:
+print("\nI queried for Tweets containing Nehru/Indira/Rajiv/Jawaharlal/Bofors")
+print("{:.2f}% of {:d} Chowkidar Tweets refer to dead Nehru-Gandhis".format(nehruvian_tweets[1]/total_tweets*100,total_tweets))
+print("\nI queried for Tweets containing Hindu/Hindutva/Hinduim/Islam/Muslim")
+print("{:.2f}% of {:d} Chowkidar Tweets refer to religion".format(religious_tweets[1]/total_tweets*100,total_tweets))
 
 
 #Lets check if we're picking up the right devnagri tweets, first time working with devnagri text
